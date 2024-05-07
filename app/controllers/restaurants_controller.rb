2aeea90998f233f721c20d842adbf710a4e07ded
Douglas Berkley <douglasmberkley@gmail.com>
2024-05-07 10:46:45 +0900

    tues morning livecode

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,63 @@
+class RestaurantsController < ApplicationController
+
+  # '/restaurants'
+  def index
+    @restaurants = Restaurant.all
+    # render 'index.html.erb'
+  end
+
+  # '/restaurants/1'
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  # '/restaurants/new'
+  def new
+    # this instance variable is JUST for the form
+    @restaurant = Restaurant.new
+  end
+
+  # we cant access from a URL. The form is sending the data here (/restaurants)
+  # THIS DOES NOT HAVE A VIEW
+  def create
+    # we need to create our instance with our STRONG params
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      # show the form with the restaurant that didnt save (not a brand new one)
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  # '/restaurants/1/edit'
+  def edit
+    # this instance variable is JUST for the form
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    # load the the instacne i want to update
+    @restaurant = Restaurant.find(params[:id])
+    # update the instance with the strong_params
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  # we cant access this one in the URL, but a link can with a delete method
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path, status: :see_other
+  end
+
+  private
+
+  def restaurant_params
+    # we are whitelisting the attributes a user can give us
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,34 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # http_verb '/path', to: 'controller#action', as: :prefix
+  # the prefix is only connected to the path
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # CRUD
+  # Read all
+  get '/restaurants', to: 'restaurants#index', as: :restaurants
+
+  # Create
+  # One page for JUST the form
+  get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # Another action to create the instance from the form data
+  post '/restaurants', to: 'restaurants#create'
+
+  # Read one
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # Update
+  # One page for JUST the form
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # Another action to update the instance from the form data
+  patch '/restaurants/:id', to: 'restaurants#update'
+
+  # Delete
+  delete '/restaurants/:id', to: 'restaurants#destroy'
 end
+
+
+# See all restaurants -> index - restaurants_path
+# See details about one restaurant -> show
+# Create a restaurant
+# Update a restaurant
+# Destroy a restaurant
